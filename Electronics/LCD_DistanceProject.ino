451d5bb2b21a1fee2c0b70096458c2fd7b8a69ec
EliasSushiClub <82659509+EliasSushiClub@users.noreply.github.com>
2021-04-16 20:35:44 +0300

    v3.0 additional features + bug fixes

--- Electronics/LCD_DistanceProject.ino
+++ Electronics/LCD_DistanceProject.ino
@@ -1,49 +1,61 @@
 #include <LiquidCrystal_I2C.h>
-#include <UltraDistSensor.h>
-#include <Wire.h>
 
-LiquidCrystal_I2C lcd(0x27, 20, 4);
-UltraDistSensor mainSensor;
-float reading;
+const int trigPin = 11;
+const int echoPin = 12;
+
+long duration;
+int distance;
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
     lcd.init();
     lcd.backlight();
-    Serial.begin(9600);
-    mainSensor.attach(12, 13);
     pinMode(2, OUTPUT); //BUZZER
     pinMode(3, OUTPUT); //GREEN
     pinMode(4, OUTPUT); //YELLOW
     pinMode(5, OUTPUT); //RED
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+    Serial.begin(9600);
 }
 
 void loop() {
-    reading = mainSensor.distanceInCm();
-    lcd.setCursor(0, 0);
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    duration = pulseIn(echoPin, HIGH);
+    distance = duration*(0.034/2);
+    
+    lcd.setCursor(2, 0);
     lcd.print("Distance: ");
-    lcd.print(reading);
+    lcd.print(distance);
 
-    if (reading < 50 && reading > 0) {
+    if (distance < 50 && distance > 0) {
         digitalWrite(3, HIGH);
-        tone(2, 10000);
-        delay(5000);
+        tone(2, 262);
+        delay(2000);
         digitalWrite(3, LOW);
         noTone(2);
     }
 
-    else if (reading > 50 && reading < 100) {
+    else if (distance > 50 && distance < 100) {
         digitalWrite(4, HIGH);
-        tone(2, 15000);
-        delay(5000);
+        tone(2, 294);
+        delay(2000);
         digitalWrite(4, LOW);
         noTone(2);
     }
 
-    else if (reading > 100 && reading < 150) {
+    else if (distance > 100 && distance < 150) {
         digitalWrite(5, HIGH);
-        tone(2, 20000);
-        delay(5000);
+        tone(2, 330);
+        delay(2000);
         digitalWrite(5, LOW);
         noTone(2);
     }
+    delayMicroseconds(1000);
+    lcd.clear();
 }
