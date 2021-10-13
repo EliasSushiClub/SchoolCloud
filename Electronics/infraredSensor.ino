b2d9b353a3e7b2cd95be0fd3cd9eb2dde45c4d46
EliasSushiClub <82659509+EliasSushiClub@users.noreply.github.com>
2021-10-13 13:21:19 +0300

    Update infraredSensor.ino

--- Electronics/infraredSensor.ino
+++ Electronics/infraredSensor.ino
@@ -1,36 +1,17 @@
 #define SOUT 12
-//#define REC 8
-#define PLAYE 9
-#define PLAYL 10
-
-void setup() {
-    pinMode(SOUT, INPUT);
-    Serial.begin(9600);
-}
-
-void loop() {
-    //delay(10000);
-    int ret = digitalRead(SOUT);
-    if (ret == 1) {
-        Serial.println("Danger Detected :(");
-        digitalWrite(PLAYE, 1);
-        delay(3000);
-        digitalWrite(PLAYE, 0);
-    }
-}
-
-
-////////
-
-#define SOUT 12
 #define REC 8
 #define PLAYE 9
 #define PLAYL 10
 
 void setup() {
     pinMode(SOUT, INPUT);
+    pinMode(REC, OUTPUT);
     Serial.begin(9600);
     Serial.println("Start Speaking in 3 seconds:");
+    for(int i=3; i=0; i--) {
+      Serial.println(i);
+      }
+    Serial.println("You have 10 seconds");
     digitalWrite(REC, 1);
     delay(10000);
     digitalWrite(REC, 0);
