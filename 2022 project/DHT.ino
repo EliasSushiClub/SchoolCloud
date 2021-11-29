023ad2ada48da6605e66e075aeedaf29bfa5f0c6
EliasSushiClub <82659509+EliasSushiClub@users.noreply.github.com>
2021-11-29 08:41:06 +0200

    dht sample

--- 2022 project/DHT.ino
+++ 2022 project/DHT.ino
@@ -1,1 +1,24 @@
+#include <dht.h>
+#include <LiquidCrystal_I2C.h>
 
+dht DHT;
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+#define DHT11_PIN 2
+
+void setup(){
+  Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+}
+
+void loop(){
+    int chk = DHT.read11(DHT11_PIN);
+    lcd.setCursor(2, 0);
+    lcd.print("Temperature = ");
+    lcd.print(DHT.temperature);
+    lcd.setCursor(2, 1);
+    lcd.print("Humidity = ");
+    lcd.print(DHT.humidity);
+    lcd.clear();
+    delay(1000);
+}
