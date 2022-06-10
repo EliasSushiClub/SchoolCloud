8a34155594d37215f64eb099e27857338221200f
EliasSushiClub <82659509+EliasSushiClub@users.noreply.github.com>
2022-06-10 11:00:02 +0300

    Update pulseMonitor.ino

--- Nia/pulseMonitor.ino
+++ Nia/pulseMonitor.ino
@@ -1,22 +1,20 @@
 int ledPin = A0;
-int sensorPin = 0;
 double alpha = 0.75;
 int period = 20;
-double change = 0.0;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin, INPUT);
   Serial.begin(115200);
 }
 
 void loop() {
   static double oldValue = 0;
   static double oldChange = 0;
-  int rawValue = analogRead(sensorPin);
+  int rawValue = analogRead(ledPin);
   double value = alpha * oldValue + (1 - alpha) * rawValue;
   Serial.print(rawValue);
   Serial.print(",");
   Serial.println(value);
   oldValue = value;
-  delay(period);
+  delay(period*10);
 }
